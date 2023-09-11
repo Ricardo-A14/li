2297c4e2821f84a783b81f688b7254f88e93cfe6
Ricardo Robayo <ricardo@DESKTOP-3OI2EQM.localdomain>
2023-09-11 09:56:04 -0500

    [ADDED] Recognition (UNSTABLE).

--- src/Components/Words/index.jsx
+++ src/Components/Words/index.jsx
@@ -1,7 +1,43 @@
 
+import { useState } from 'react';
+
 import './Word.css';
 
+const recognition = new webkitSpeechRecognition();
+
+recognition.continuous = true;
+recognition.lang = 'en-US';
+recognition.interimResult = false;
+
+
 const Word = ({ word, index }) => {
+
+    const [handleWord, setHandleWord] = useState(null);
+
+
+    const empezar = (word) => {
+
+        recognition.start();
+
+        recognition.onresult = (event) => {
+            const text = event.results[0][0].transcript;
+            console.log(text);
+        }
+
+        setTimeout(() => {
+            recognition.stop();
+
+            setTimeout(() => {
+                recognition.abort();
+
+            }, 1000);
+        }, 3000);
+
+    }
+
+
+
+
     return (
         <div className='word_c'>
             <div className="word">
@@ -9,6 +45,7 @@ const Word = ({ word, index }) => {
                 <span>{word.pronunciation}</span>
                 <div className='meaning_word'>
                     <div className='defi'>{word.definition}</div>
+                    <button className='talk' onClick={() => empezar(word.word)}>Talk</button>
                     <div className='index'>{index}</div>
                 </div>
             </div>
