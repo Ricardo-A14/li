2c0ad533c03bcf842813d91714ebec92786fdb66
Ricardo Robayo <ricardo@DESKTOP-3OI2EQM.localdomain>
2023-10-29 12:11:03 -0500

    [ADDED] Storing notes in localStorage (Stable).

--- src/Components/Notes/index.jsx
+++ src/Components/Notes/index.jsx
@@ -1,9 +1,32 @@
+import { useState, useEffect } from "react";
 import "./Notes.css";
 
 const Notes = () => {
+
+  const [notes, setNotes] = useState('');
+
+  useEffect(() => {
+    const getValue = localStorage.getItem('notes');
+    if (getValue) {
+      setNotes(getValue);
+    }
+  }, []);
+
+  const HandleNotes = (event) => {
+    const newValue = event.target.value;
+    setNotes(newValue);
+    localStorage.setItem('notes', newValue);
+  }
+
   return (
     <div className="notes_container">
-      <textarea className="textArea" cols="30" rows="10" placeholder="Escribe tus notas."></textarea>
+      <textarea
+        value={notes}
+        className="textArea"
+        cols="30" rows="10" placeholder="Escribe tus notas."
+        onChange={HandleNotes}
+      >
+      </textarea>
     </div>
   );
 };
